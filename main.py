8539907d4ad2c21fab8ca346d61586fb8433859f
Minipatata <ashleynaemmalee3827@ele.cssda.ca>
2026-04-19 12:30:29 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+num_user=int(input("Please, enter any desired number of your choice :"))
+if num_user&num_user-1==0 and (num_user& 0X55555555) !=0:
+    print("This is the power of 4 :)")
+else:
+    print("This is NOT the power of 4")
